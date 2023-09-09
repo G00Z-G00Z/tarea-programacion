8c2b530e7d6ba105388fa62bbf99d35524a5ec31
Jesus Alejandro Izaguirre Garza <jesusalejandroizaguirre@gmail.com>
2023-09-08 17:28:12 -0700

    Correciones FizzBuzz
    Corregi el numero primo y cambie de orden

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,12 @@
-for fizzbuzz in range(50):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+for fizzbuzz in range(30):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 6 == 0:
         print("fizzbuzz")
         continue
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
-        continue
-    elif fizzbuzz % 5 == 0:
+    elif fizzbuzz % 6 == 0:
         print("buzz")
         continue
+    elif fizzbuzz % 3 == 0:
+        print("Fizz")
+        continue
     print(fizzbuzz)
+
